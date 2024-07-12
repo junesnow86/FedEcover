0aa2729276aee840d490d8fb426e86e5759e931f
junesnow86 <junliang@mail.ustc.edu.cn>
2024-07-12 16:04:57 +0000

    Distinguish  train_transform and test_transform

--- test_aggregation_CNN_CIFAR10.py
+++ test_aggregation_CNN_CIFAR10.py
@@ -86,20 +86,27 @@ BATCH_SIZE = 128
 EPOCHS = 10
 LEARNING_RATE = 0.001
 
-transform = transforms.Compose(
+train_transform = transforms.Compose(
     [
-        # transforms.RandomHorizontalFlip(),
-        # transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomCrop(32, padding=4),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ]
+)
+
+test_transform = transforms.Compose(
+    [
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ]
 )
 
 train_dataset = torchvision.datasets.CIFAR10(
-    root="./data", train=True, download=False, transform=transform
+    root="./data", train=True, download=False, transform=train_transform
 )
 test_dataset = torchvision.datasets.CIFAR10(
-    root="./data", train=False, download=False, transform=transform
+    root="./data", train=False, download=False, transform=test_transform
 )
 
 train_loader = DataLoader(
