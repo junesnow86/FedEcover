114553fe38674d1079e7dd568d45272b47a1d9db
junesnow86 <junliang@mail.ustc.edu.cn>
2024-07-13 07:37:46 +0000

    Add heterogeneous experiment

--- modules/models.py
+++ modules/models.py
@@ -1,24 +1,4 @@
 import torch.nn as nn
-import torch.nn.functional as F
-
-
-class SimpleCNN(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=5)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=5)
-        # 全连接层，输入特征数为64*(图像尺寸-8)*(图像尺寸-8)，输出特征数为1024
-        # 图像尺寸经过两次卷积后，每次卷积边缘减少4（卷积核为5x5），所以总共减少8
-        self.fc1 = nn.Linear(64 * (32 - 8) * (32 - 8), 1024)
-        self.fc2 = nn.Linear(1024, 10)
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = x.view(-1, 64 * (32 - 8) * (32 - 8))
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
 
 
 class CNN(nn.Module):
@@ -26,19 +6,16 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
         self.layer1 = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
         self.layer2 = nn.Sequential(
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
         self.layer3 = nn.Sequential(
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
--- modules/utils.py
+++ modules/utils.py
@@ -3,6 +3,8 @@ from typing import Dict, List
 import numpy as np
 import torch
 
+from modules.models import CNN
+
 
 def prune_linear_layer(linear_layer, pruned_indices=None):
     """
@@ -301,4 +303,163 @@ def calculate_model_size(model):
     print(f"Total parameters: {total_params}")
     print(
         f"Memory Usage: {memory_bytes} bytes ({memory_kilobytes:.2f} KB / {memory_megabytes:.2f} MB)"
+    )
+
+
+# Training function
+def train(model, device, train_loader, optimizer, criterion, epochs=30):
+    model.train()
+    for epoch in range(epochs):
+        total_loss = 0
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data, target = data.to(device), target.to(device)
+            optimizer.zero_grad()
+            output = model(data)
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+
+        avg_loss = total_loss / len(train_loader)
+        print(f"Train Epoch: {epoch}/{epochs} \tAverage Loss: {avg_loss:.6f}")
+
+
+# Testing function
+def test(model, device, test_loader, criterion, num_classes=10):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    class_correct = dict()
+    class_total = dict()
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += criterion(output, target).item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+            for label, prediction in zip(target.view_as(pred), pred):
+                if label.item() not in class_correct:
+                    class_correct[label.item()] = 0
+                if label.item() not in class_total:
+                    class_total[label.item()] = 0
+
+                if label == prediction:
+                    class_correct[label.item()] += 1
+
+                class_total[label.item()] += 1
+
+    test_loss /= len(test_loader.dataset)
+    accuracy = correct / len(test_loader.dataset)
+    class_accuracy = {
+        cls: class_correct[cls] / class_total[cls] for cls in sorted(class_total)
+    }
+    return test_loss, accuracy, class_accuracy
+
+
+def prune_cnn(original_cnn, dropout_rate=0.5):
+    conv1 = original_cnn.layer1[0]
+    num_output_channels_to_prune_conv1 = int(conv1.out_channels * dropout_rate)
+    output_indices_to_prune_conv1 = np.random.choice(
+        conv1.out_channels, num_output_channels_to_prune_conv1, replace=False
+    )
+    indices_to_prune_conv1 = {"output": output_indices_to_prune_conv1}
+    pruned_layer1 = prune_conv_layer(conv1, indices_to_prune_conv1)
+
+    conv2 = original_cnn.layer2[0]
+    num_output_channels_to_prune_conv2 = int(conv2.out_channels * dropout_rate)
+    output_indices_to_prune_conv2 = np.random.choice(
+        conv2.out_channels, num_output_channels_to_prune_conv2, replace=False
+    )
+    indices_to_prune_conv2 = {
+        "input": output_indices_to_prune_conv1,
+        "output": output_indices_to_prune_conv2,
+    }
+    pruned_layer2 = prune_conv_layer(conv2, indices_to_prune_conv2)
+
+    conv3 = original_cnn.layer3[0]
+    num_output_channels_to_prune_conv3 = int(conv3.out_channels * dropout_rate)
+    output_indices_to_prune_conv3 = np.random.choice(
+        conv3.out_channels, num_output_channels_to_prune_conv3, replace=False
+    )
+    indices_to_prune_conv3 = {
+        "input": output_indices_to_prune_conv2,
+        "output": output_indices_to_prune_conv3,
+    }
+    pruned_layer3 = prune_conv_layer(conv3, indices_to_prune_conv3)
+
+    fc = original_cnn.fc
+    input_indices_to_prune_fc = []
+    for channel_index in output_indices_to_prune_conv3:
+        start_index = channel_index * 4 * 4
+        end_index = (channel_index + 1) * 4 * 4
+        input_indices_to_prune_fc.extend(list(range(start_index, end_index)))
+    input_indices_to_prune_fc = np.sort(input_indices_to_prune_fc)
+    indices_to_prune_fc = {"input": input_indices_to_prune_fc}
+    pruned_fc = prune_linear_layer(fc, indices_to_prune_fc)
+
+    scale_factor = 1 / (1 - dropout_rate)
+    pruned_layer1.weight.data *= scale_factor
+    pruned_layer2.weight.data *= scale_factor
+    pruned_layer3.weight.data *= scale_factor
+    pruned_fc.weight.data *= scale_factor
+
+    pruned_cnn = CNN()
+    pruned_cnn.layer1[0] = pruned_layer1
+    pruned_cnn.layer2[0] = pruned_layer2
+    pruned_cnn.layer3[0] = pruned_layer3
+    pruned_cnn.fc = pruned_fc
+
+    return (
+        pruned_cnn,
+        indices_to_prune_conv1,
+        indices_to_prune_conv2,
+        indices_to_prune_conv3,
+        indices_to_prune_fc,
+    )
+
+
+def aggregate_cnn(
+    original_cnn,
+    pruned_cnn_list,
+    num_samples_list,
+    dropout_rate_list,
+    indices_to_prune_dict: Dict[str, list],
+):
+    indices_to_prune_conv1 = indices_to_prune_dict["indices_to_prune_conv1"]
+    indices_to_prune_conv2 = indices_to_prune_dict["indices_to_prune_conv2"]
+    indices_to_prune_conv3 = indices_to_prune_dict["indices_to_prune_conv3"]
+    indices_to_prune_fc = indices_to_prune_dict["indices_to_prune_fc"]
+
+    for i in range(len(pruned_cnn_list)):
+        scale_factor = 1 - dropout_rate_list[i]
+        pruned_cnn_list[i].layer1[0].weight.data *= scale_factor
+        pruned_cnn_list[i].layer2[0].weight.data *= scale_factor
+        pruned_cnn_list[i].layer3[0].weight.data *= scale_factor
+        pruned_cnn_list[i].fc.weight.data *= scale_factor
+
+    aggregate_conv_layers(
+        original_cnn.layer1[0],
+        [pruned_cnn.layer1[0] for pruned_cnn in pruned_cnn_list],
+        indices_to_prune_conv1,
+        num_samples_list,
+    )
+    aggregate_conv_layers(
+        original_cnn.layer2[0],
+        [pruned_cnn.layer2[0] for pruned_cnn in pruned_cnn_list],
+        indices_to_prune_conv2,
+        num_samples_list,
+    )
+    aggregate_conv_layers(
+        original_cnn.layer3[0],
+        [pruned_cnn.layer3[0] for pruned_cnn in pruned_cnn_list],
+        indices_to_prune_conv3,
+        num_samples_list,
+    )
+    aggregate_linear_layers(
+        original_cnn.fc,
+        [pruned_cnn.fc for pruned_cnn in pruned_cnn_list],
+        indices_to_prune_fc,
+        num_samples_list,
     )
