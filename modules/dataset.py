9c3f12cea787aac19602fb4c38baac1e2650a4f7
junesnow86 <junliang@mail.ustc.edu.cn>
2024-08-28 11:52:26 +0000

    Add dataset definition

---
+++ modules/dataset.py
@@ -0,0 +1,23 @@
+import torch
+from torch.utils.data import Dataset
+
+
+# Create input-output pairs for next word prediction
+class NextWordPredictionDataset(Dataset):
+    def __init__(self, tokenized_data):
+        self.input_ids = tokenized_data["input_ids"]
+        self.attention_mask = tokenized_data["attention_mask"]
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        input_ids = torch.tensor(self.input_ids[idx])
+        labels = input_ids.clone()
+        labels[:-1] = input_ids[1:]
+        labels[-1] = -100  # Ignore the last token
+        return {
+            "input_ids": input_ids,
+            "attention_mask": torch.tensor(self.attention_mask[idx]),
+            "labels": labels,
+        }
