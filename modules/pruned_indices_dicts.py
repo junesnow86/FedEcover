e255426560cf3c70e24af7ea72855b07d0cd6745
junesnow86 <junliang@mail.ustc.edu.cn>
2024-09-08 15:17:12 +0000

    Add some pruned_indices_dict class

---
+++ modules/pruned_indices_dicts.py
@@ -0,0 +1,34 @@
+from typing import Union
+
+import numpy as np
+
+
+class LayerPrunedIndicesDict(dict):
+    def __setitem__(self, key: str, value: Union[list, np.ndarray]) -> None:
+        if key not in ["input", "output"]:
+            raise ValueError("Key must be either 'input' or 'output'")
+        if not isinstance(value, (list, np.ndarray)):
+            raise ValueError("Value must be a list or numpy array")
+        super().__setitem__(key, value)
+
+
+class BlockPrunedIndicesDict(dict):
+    def __setitem__(
+        self, key: str, value: Union[LayerPrunedIndicesDict, "BlockPrunedIndicesDict"]
+    ) -> None:
+        if not isinstance(value, (LayerPrunedIndicesDict, BlockPrunedIndicesDict)):
+            raise ValueError(
+                "Value must be an instance of LayerPrunedIndicesDict or BlockPrunedIndicesDict"
+            )
+        super().__setitem__(key, value)
+
+
+class ModelPrunedIndicesDict(dict):
+    def __setitem__(
+        self, key: str, value: Union[LayerPrunedIndicesDict, BlockPrunedIndicesDict]
+    ) -> None:
+        if not isinstance(value, (LayerPrunedIndicesDict, BlockPrunedIndicesDict)):
+            raise TypeError(
+                "Value must be a LayerPrunedIndicesDict or BlockPrunedIndicesDict"
+            )
+        super().__setitem__(key, value)
