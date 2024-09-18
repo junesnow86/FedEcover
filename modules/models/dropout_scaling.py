d5f845913cf74b333fc384dc4dfe2194932c5144
junesnow86 <junliang@mail.ustc.edu.cn>
2024-09-18 12:40:24 +0000

    Delete one unused line

--- modules/models/dropout_scaling.py
+++ modules/models/dropout_scaling.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 class DropoutScaling(nn.Module):
     def __init__(self, p):
         super().__init__()
-        self.p = p
         self.scale = 1.0 / (1.0 - p)
 
     def forward(self, x):
