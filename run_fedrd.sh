61f9a6263295d05832258b0a3d86d12dc20da44c
junesnow86 <junliang@mail.ustc.edu.cn>
2024-09-25 02:52:17 +0000

    Add migrating files

--- run_fedrd.sh
+++ run_fedrd.sh
@@ -2,4 +2,4 @@ sleep 60
 sh /home/notebook/code/personal/S9056161/init.sh
 source ~/.bashrc
 conda activate rd
-python homo_training_image.py --method fedavg --model resnet --distribution non-iid --alpha 0.5 --epochs 10 --save-dir results/0919 >> logs/0919/fedavg_resnet_cifar10_alpha0.5_epochs10.log
+python partial_training_image.py --method fedrd --model resnet --distribution non-iid --alpha 0.5 --epochs 20 --save-dir results/0919 >> logs/0919/fedrd_resnet_cifar10_alpha0.5_epochs20.log
