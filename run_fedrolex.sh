9f85fb6a0762634288cd022ed47cde1ded1c1694
junesnow86 <junliang@mail.ustc.edu.cn>
2024-09-30 02:12:56 +0000

    Remove useless sh files

--- run_fedrolex.sh
+++
@@ -1,5 +0,0 @@
-sleep 60
-sh /home/notebook/code/personal/S9056161/init.sh
-source ~/.bashrc
-conda activate rd
-python partial_training_image.py --method fedrolex --model resnet --distribution non-iid --alpha 0.5 --epochs 20 --save-dir results/0919 >> logs/0919/fedrolex_resnet_cifar10_alpha0.5_epochs20.log
--- run_rdbagging.sh
+++
@@ -1,5 +0,0 @@
-sleep 60
-sh /home/notebook/code/personal/S9056161/init.sh
-source ~/.bashrc
-conda activate rd
-python partial_training_image.py --method rdbagging --model resnet --distribution non-iid --alpha 0.5 --epochs 20 --save-dir results/0919 >> logs/0919/rdbagging_resnet_cifar10_alpha0.5_epochs20.log
