3e731d946243b29e1b453c9de415b43c3350c173
junesnow86 <junliang@mail.ustc.edu.cn>
2024-10-23 08:56:15 +0000

    Remove files no need anymore

--- run_homo_training.sh
+++
@@ -1,8 +0,0 @@
-# sleep 60
-# /home/notebook/code/personal/S9056161/miniconda3/bin/conda init bash
-# /home/notebook/code/personal/S9056161/miniconda3/bin/conda init zsh
-# source ~/.bashrc
-# source ~/.zshrc
-# conda activate rd-pruning
-pip install -r requirements.txt
-python homo_training_image.py --method $1 --model $2 --dataset $3 --distribution $4 --alpha $5 --rounds $6 --epochs $7 --batch-size $8 >> $9
