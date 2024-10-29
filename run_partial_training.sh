341fa75cfc627411c9a7b9410490948753d60752
junesnow86 <junliang@mail.ustc.edu.cn>
2024-10-29 11:54:11 +0000

    Add upgrade pip command

--- run_partial_training.sh
+++ run_partial_training.sh
@@ -4,5 +4,6 @@
 # source ~/.bashrc
 # source ~/.zshrc
 # conda activate rd-pruning
+pip install --upgrade pip
 pip install -r requirements.txt
 python partial_training_image.py --method ${1} --model ${2} --dataset ${3} --distribution ${4} --num-clients ${5} --client-select-ratio ${6} --local-train-ratio ${7} --rounds ${8} --epochs ${9} --batch-size ${10} --lr ${11} --weight-decay ${12} --eta_g ${13} --dynamic-eta_g ${14} --norm-type ${15} --client-capacity-distribution ${16} --global-lr-decay ${17} --gamma ${18} --data-augmentation ${19} >> ${20}
