ca7c610c77d295cb9d5b7553165b05f2f86abeca
junesnow86 <junliang@mail.ustc.edu.cn>
2024-11-05 07:14:58 +0000

    Remove NextwordDataset

--- modules/data/dataset.py
+++ modules/data/dataset.py
@@ -1,7 +1,7 @@
 import os
 
-import torch
-from datasets.arrow_dataset import Dataset as HFDataset
+# import torch
+# from datasets.arrow_dataset import Dataset as HFDataset
 from PIL import Image
 from torch.utils.data import Dataset
 
@@ -64,22 +64,22 @@ class TinyImageNet(Dataset):
 
 
 # Create input-output pairs for next word prediction
-class NextWordPredictionDataset(Dataset):
-    def __init__(self, tokenized_data: HFDataset, padding_index: int = 0):
-        self.input_ids = tokenized_data["input_ids"]
-        self.attention_mask = tokenized_data["attention_mask"]
-        self.padding_index = padding_index
+# class NextWordPredictionDataset(Dataset):
+#     def __init__(self, tokenized_data: HFDataset, padding_index: int = 0):
+#         self.input_ids = tokenized_data["input_ids"]
+#         self.attention_mask = tokenized_data["attention_mask"]
+#         self.padding_index = padding_index
 
-    def __len__(self):
-        return len(self.input_ids)
+#     def __len__(self):
+#         return len(self.input_ids)
 
-    def __getitem__(self, idx):
-        input_ids = torch.tensor(self.input_ids[idx])
-        labels = input_ids.clone()
-        labels[:-1] = input_ids[1:]
-        labels[-1] = self.padding_index
-        return {
-            "input_ids": input_ids,
-            "attention_mask": torch.tensor(self.attention_mask[idx]),
-            "labels": labels,
-        }
+#     def __getitem__(self, idx):
+#         input_ids = torch.tensor(self.input_ids[idx])
+#         labels = input_ids.clone()
+#         labels[:-1] = input_ids[1:]
+#         labels[-1] = self.padding_index
+#         return {
+#             "input_ids": input_ids,
+#             "attention_mask": torch.tensor(self.attention_mask[idx]),
+#             "labels": labels,
+#         }
