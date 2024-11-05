24de24309b8790f4861239432f2b16824297cae0
junesnow86 <junliang@mail.ustc.edu.cn>
2024-11-05 12:30:17 +0000

    Fix dataset import bug

--- modules/data/__init__.py
+++ modules/data/__init__.py
@@ -1,5 +1,7 @@
-from .dataset import NextWordPredictionDataset, TinyImageNet
+# from .dataset import NextWordPredictionDataset, TinyImageNet
+from .dataset import TinyImageNet
 from .distribution import create_non_iid_data
 
 
-__all__ = ["NextWordPredictionDataset", "create_non_iid_data", "TinyImageNet"]
+# __all__ = ["NextWordPredictionDataset", "create_non_iid_data", "TinyImageNet"]
+__all__ = ["create_non_iid_data", "TinyImageNet"]
