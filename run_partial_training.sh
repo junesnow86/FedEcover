34e0f489d2323181aeaaece47b3309239c1cad00
junesnow86 <junliang@mail.ustc.edu.cn>
2024-11-09 07:21:47 +0000

    Upload tiny-iamagenet downloading instruction and a commonly used training command

--- run_partial_training.sh
+++ run_partial_training.sh
@@ -4,6 +4,7 @@
 # source ~/.bashrc
 # source ~/.zshrc
 # conda activate rd-pruning
+# taskset -c 0-4 python partial_training_image.py --method fedrd --model cnn --dataset cifar10 --distribution alpha0.5 --num-clients 10 --client-select-ratio 1.0 --local-train-ratio 1.0 --rounds 300 --epochs 2 --batch-size 64 --lr 0.001 --weight-decay 0.0001 --eta_g 1.0 --dynamic-eta_g False --norm-type sbn --client-capacity-distribution 2 --global-lr-decay True --gamma 0.9 --data-augmentation True >> logs/1108/fedrd-cnn-cifar10-alpha0.5-capacity2-10clients.log
 pip install --upgrade pip
 pip install -r requirements.txt
 python partial_training_image.py --method ${1} --model ${2} --dataset ${3} --distribution ${4} --num-clients ${5} --client-select-ratio ${6} --local-train-ratio ${7} --rounds ${8} --epochs ${9} --batch-size ${10} --lr ${11} --weight-decay ${12} --eta_g ${13} --dynamic-eta_g ${14} --norm-type ${15} --client-capacity-distribution ${16} --global-lr-decay ${17} --gamma ${18} --data-augmentation ${19} >> ${20}
